5dd7a38b287a415ab65cd63b56d08a033ca862f3
valeryO <olennval@mail.ru>
2022-08-06 22:49:28 +0700

    использование методов для ввода и вывода массива

---
+++ Example011_vvod_vivod_mas/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+
+{
+    int dlina = collection.Length;
+    int index = 0;
+    while (index < dlina)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintMas(int[] col)
+{
+    int dlina2 = col.Length;
+    int index = 0;
+    while (index < dlina2)
+    {
+        Console.WriteLine(col[index]);
+        index++;
+    }
+}
+
+int[] mas =  new int[10];
+FillArray(mas);
+PrintMas(mas);
